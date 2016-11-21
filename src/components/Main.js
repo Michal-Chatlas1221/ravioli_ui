5cb956f2860e1f7fd3260f6777c159af22e37374
Michał Chatłas <michal.chatlas@espeo.eu>
2016-11-21 10:52:10 +0100

    Updated texts

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
 export default class Main extends React.Component {
-
     render() {
         return (
             <div>
                 <div>
                     <h2>Ravioli is a state of the art distibuted computing system</h2>
                     <p>
-                        Ravioli allows you to use our pool of associated websites and blogs for your own
-                        calculations without the need for own resources. And the computing power providers
+                        Ravioli allows you to use our pool of associated websites and blogs for your
+                        calculations without the need for own resources. We provide you everything, and the computing power providers
                         will be unaware of this. *DEMONIC LAUGHTER*
                     </p>
                 </div>
